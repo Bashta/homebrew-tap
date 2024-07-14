e42384ac7690d5c2d69d3fbd3bafc3dd66586203
Erison Veshi <erisonve@gmail.com>
2024-07-14 18:45:13 +0200

    Update swiftfilescombiner.rb

--- swiftfilescombiner.rb
+++ swiftfilescombiner.rb
@@ -1,8 +1,8 @@
 class Swiftfilescombiner < Formula
   desc "CLI utility to combine Swift files"
   homepage "https://github.com/bashta/SwiftFilesCombiner"
-  url "https://github.com/Bashta/FilesCombiner/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "24599eb25f5dffb27d18d2f0e0074dda328272c6c3a4ca560f84559dac593deb"
+  url "https://github.com/Bashta/FilesCombiner/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "bc837530346a14817aeed56de3d0ce9ebcc99305aec9e0427cd781c4a2c2d562"
   license "MIT"
 
   depends_on xcode: ["15.0", :build]
